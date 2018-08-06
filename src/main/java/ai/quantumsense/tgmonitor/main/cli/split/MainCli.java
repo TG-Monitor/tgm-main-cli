5e645ce08d1aaec493f714468331354310f569c4
Daniel Weibel <danielmweibel@gmail.com>
2018-08-06 19:18:40 +0800

    Add SLF4J logging

---
+++ src/main/java/ai/quantumsense/tgmonitor/main/cli/split/MainCli.java
@@ -0,0 +1,20 @@
+package ai.quantumsense.tgmonitor.main.cli.split;
+
+import ai.quantumsense.tgmonitor.cli.Cli;
+import ai.quantumsense.tgmonitor.ipc.UiEndpoint;
+import ai.quantumsense.tgmonitor.ipc.messenger.RabbitMqUiMessenger;
+import ai.quantumsense.tgmonitor.ipc.messenger.serializer.JsonSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainCli {
+    private static Logger logger = LoggerFactory.getLogger(MainCli.class);
+
+    private static final String VERSION = "0.0.5";
+
+    public static void main(String[] args) {
+        UiEndpoint uiEndpoint = new UiEndpoint(new RabbitMqUiMessenger(new JsonSerializer()));
+        Cli cli = new Cli(uiEndpoint, VERSION);
+        cli.launch();
+    }
+}
