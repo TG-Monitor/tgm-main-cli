42b359a9ad07deb7b84b5eed4f456407139c9e0e
Daniel Weibel <danielmweibel@gmail.com>
2018-08-07 17:48:44 +0800

    Add log statements

--- src/main/java/ai/quantumsense/tgmonitor/main/cli/split/MainCli.java
+++ src/main/java/ai/quantumsense/tgmonitor/main/cli/split/MainCli.java
@@ -13,7 +13,9 @@ public class MainCli {
     private static final String VERSION = "0.0.5";
 
     public static void main(String[] args) {
+        logger.debug("Creating UI Endpoint");
         UiEndpoint uiEndpoint = new UiEndpoint(new RabbitMqUiMessenger(new JsonSerializer()));
+        logger.debug("Creating and launching CLI");
         Cli cli = new Cli(uiEndpoint, VERSION);
         cli.launch();
     }
